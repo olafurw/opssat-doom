4e5ced1b598d944a90b712f9706e5b7cd66cc59e
Olafur Waage <olafurw@gmail.com>
2023-12-21 00:26:17 +0100

    Demo checker.

--- test-run.sh
+++ test-run.sh
@@ -1,1 +1,10 @@
-./src/chocolate-doom -nographics -nosound -nograbmouse -iwad demos/doom.wad -statdump x.txt -cdemo demos/e1m7-607
+./src/chocolate-doom -nographics -nosound -nograbmouse -iwad demos/doom.wad -statdump x.txt -cdemo demos/e1m7-607 2>&1 > /dev/null;
+if diff --strip-trailing-cr x.txt demos/e1m7-607.txt; then
+    echo "OK - demos/e1m7-607";
+    rm x.txt;
+    exit 0;
+else
+    echo "ERROR - demos/e1m7-607";
+    rm x.txt;
+    exit 1;
+fi
