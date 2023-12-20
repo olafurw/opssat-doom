fe9dd026d787419dc74f29618d39636abb0e22d7
Olafur Waage <olafurw@gmail.com>
2023-12-21 00:45:50 +0100

    Adding more demo checks.

---
+++ demos/README.md
@@ -0,0 +1,8 @@
+# DOOM tests
+
+A set of demos used to test if everything is functioning ok.
+
+Some demos are from this excellent resource: http://www.classicdoom.com/odddemos.htm
+Some demos are from the compet-n and SDA archive: https://archive.org/details/competn
+
+Much thanks to the classic doom and doom speed demo archive community. Without their knowledge and hard work, this would have not have been possible.
--- test-run.sh
+++ test-run.sh
@@ -1,10 +1,46 @@
-./src/chocolate-doom -nographics -nosound -nograbmouse -iwad demos/doom.wad -statdump x.txt -cdemo demos/e1m7-607 2>&1 > /dev/null;
-if diff --strip-trailing-cr x.txt demos/e1m7-607.txt; then
+./src/chocolate-doom -nographics -nosound -nograbmouse -iwad demos/doom.wad -statdump compare.txt -cdemo demos/e1m7-607 2>&1 > /dev/null;
+if diff --strip-trailing-cr compare.txt demos/e1m7-607.txt; then
     echo "OK - demos/e1m7-607";
-    rm x.txt;
-    exit 0;
 else
     echo "ERROR - demos/e1m7-607";
-    rm x.txt;
+    rm compare.txt;
     exit 1;
 fi
+
+./src/chocolate-doom -nographics -nosound -nograbmouse -iwad demos/doom.wad -statdump compare.txt -cdemo demos/impfight 2>&1 > /dev/null;
+if diff --strip-trailing-cr compare.txt demos/impfight.txt; then
+    echo "OK - demos/impfight";
+else
+    echo "ERROR - demos/impfight";
+    rm compare.txt;
+    exit 1;
+fi
+
+./src/chocolate-doom -nographics -nosound -nograbmouse -iwad demos/doom.wad -statdump compare.txt -cdemo demos/m1-fast 2>&1 > /dev/null;
+if diff --strip-trailing-cr compare.txt demos/m1-fast.txt; then
+    echo "OK - demos/m1-fast";
+else
+    echo "ERROR - demos/m1-fast";
+    rm compare.txt;
+    exit 1;
+fi
+
+./src/chocolate-doom -nographics -nosound -nograbmouse -iwad demos/doom.wad -statdump compare.txt -cdemo demos/m1-normal 2>&1 > /dev/null;
+if diff --strip-trailing-cr compare.txt demos/m1-normal.txt; then
+    echo "OK - demos/m1-normal";
+else
+    echo "ERROR - demos/m1-normal";
+    rm compare.txt;
+    exit 1;
+fi
+
+./src/chocolate-doom -nographics -nosound -nograbmouse -iwad demos/doom.wad -statdump compare.txt -cdemo demos/m1-simple 2>&1 > /dev/null;
+if diff --strip-trailing-cr compare.txt demos/m1-simple.txt; then
+    echo "OK - demos/m1-simple";
+else
+    echo "ERROR - demos/m1-simple";
+    rm compare.txt;
+    exit 1;
+fi
+
+rm compare.txt;
