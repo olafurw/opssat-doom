ac0e31e5cb7ef6fb23c63e9c6af9573e20052a68
Olafur Waage <olafurw@gmail.com>
2024-01-01 18:41:18 +0100

    Updating test script.

--- src/test.sh
+++ src/test.sh
@@ -1,1 +1,1 @@
-rm -rf output && mkdir output && ./build-32.sh && ./bin/opssat-doom -nosound -nomusic -nosfx -nodraw -iwad ../demos/doom.wad -playdemo ../demos/m1-fast
+rm -rf output && mkdir output && ./build-32.sh && ./bin/opssat-doom -nosound -nomusic -nosfx -nodraw -iwad ../demos/doom.wad -playdemo ../demos/e1m7-607
