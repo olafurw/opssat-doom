7044806518f23d020f8574f07c98395f039f2edb
Olafur Waage <olafurw@gmail.com>
2024-01-02 03:58:14 +0100

    Adding more test scripts.

--- run-64.sh
+++ run-64.sh
@@ -1,4 +1,2 @@
-echo "===========================================================================";
 rm -rf output && mkdir output;
-./build-64.sh;
 ./src/bin/opssat-doom -nosound -nomusic -nosfx -nodraw -iwad demos/doom.wad -playdemo demos/m1-fast -statdump toGround/x.txt;
