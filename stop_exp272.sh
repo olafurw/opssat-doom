68ef622ded374a4c9a7ec7a1f1b3e4a66c01e21a
Georges Labreche <georges@tanagraspace.com>
2024-01-07 04:33:25 +0000

    issue #8 include seu simulation

--- stop_exp272.sh
+++ stop_exp272.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
 # The DOOM experiment will stop on its own.
+# But kill the game process in case it's hanging.
+
+# Loop through each line of the ps aux output that matches 'opssat-doom'
+for pid in $(ps aux | grep -i "opssat-doom" | grep -v "grep" | awk '{print $2}')
+do
+  echo "Killing process with PID: $pid"
+  kill -9 $pid
+done
+
 # Qapla'
 exit 0
