f155ba11fa4f2a7938c1704a0376c85ca924b443
Olafur Waage <olafurw@gmail.com>
2024-01-22 00:45:05 +0100

    Finall fixing W_LumpLength error.

--- run-64.sh
+++ run-64.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 demos_folder="demos/rng-0"
-demos_shareware="e1m1"
+demos_shareware="zero-e1m1 zero-e1m2 zero-e1m3 zero-e1m4 zero-e1m5 zero-e1m6 zero-e1m7"
 
 for demo in $demos_shareware
 do
-    ./src/bin/opssat-doom -nosound -nomusic -nosfx -runid 1 -iwad demos/doom.wad -cdemo $demos_folder/$demo -statdump toGround/run_diff.txt;
+    ./src/bin/opssat-doom -nosound -nomusic -nosfx -nodraw -runid 3 -iwad demos/doom.wad -cdemo $demos_folder/$demo;
     echo $demo;
 done
