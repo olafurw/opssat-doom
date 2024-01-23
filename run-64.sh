11f6851f5023f57ec3571e71ad9c7a4a89d87e84
Olafur Waage <olafurw@gmail.com>
2024-01-23 01:39:19 +0100

    Recording a demo with rng 1.

--- run-64.sh
+++ run-64.sh
@@ -7,7 +7,7 @@ statdump_filepath=toGround/run-64.txt
 
 for demo in $demos_shareware
 do
-    ./src/bin/opssat-doom -nosound -nomusic -nosfx -nodraw -runid 1 -iwad demos/doom.wad -cdemo $demos_folder/$demo -statdump toGround/run-64.txt >> toGround/doom.log 2>&1;
+    ./src/bin/opssat-doom -nosound -nomusic -nosfx -nodraw -runid 1 -longtics -iwad demos/doom.wad -cdemo $demos_folder/$demo -statdump toGround/run-64.txt >> toGround/doom.log 2>&1;
     if [ -f "${statdump_filepath}" ]; then
         if diff ${statdump_filepath} ${demos_folder}/${demo}.txt; then
             result="OK"
