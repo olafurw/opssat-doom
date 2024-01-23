c0d60cea3b06c38ee52ec697becedec25d233ab8
Olafur Waage <olafurw@gmail.com>
2024-01-23 20:23:52 +0100

    Fixing run

--- run-32.sh
+++ run-32.sh
@@ -1,2 +1,2 @@
-source /home/user/poky_sdk/environment-setup-cortexa8hf-neon-poky-linux-gnueabi;
-qemu-arm src/bin/opssat-doom -nosound -nomusic -nosfx -nodraw -runid 3 -iwad demos/doom-earth.wad -cdemo demos/zero-e1m1-long -statdump toGround/x.txt;
+source ~/poky_sdk/environment-setup-cortexa8hf-neon-poky-linux-gnueabi;
+qemu-arm src/bin/opssat-doom -nosound -nomusic -nosfx -nodraw -runid 1 -longtics -iwad demos/doom.wad -cdemo demos/zero-e1m1-long -statdump toGround/x.txt;
