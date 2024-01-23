23c9a3b582aa30ad8a7eeeda8dafccf77f26fbec
Olafur Waage <olafurw@gmail.com>
2024-01-23 21:05:05 +0100

    Running all on 32

--- run-32.sh
+++ run-32.sh
@@ -1,3 +1,23 @@
+#!/bin/bash
 source ~/poky_sdk/environment-setup-cortexa8hf-neon-poky-linux-gnueabi;
+
+rm -rf toGround;
 mkdir -p toGround;
-qemu-arm src/bin/opssat-doom -nosound -nomusic -nosfx -runid 1 -longtics -iwad demos/doom.wad -cdemo demos/zero-e1m1-long -statdump toGround/x.txt;
+
+demos_folder="demos/rng-0"
+demos_shareware="zero-e1m1 zero-e1m2 zero-e1m3 zero-e1m4 zero-e1m5 zero-e1m6 zero-e1m7"
+
+statdump_filepath=toGround/run-64.txt
+
+for demo in $demos_shareware
+do
+    qemu-arm src/bin/opssat-doom -nosound -nomusic -nosfx -runid 1 -longtics -iwad demos/doom.wad -cdemo $demos_folder/$demo -statdump toGround/run-64.txt >> toGround/doom.log 2>&1;
+    if [ -f "${statdump_filepath}" ]; then
+        if diff ${statdump_filepath} ${demos_folder}/${demo}.txt; then
+            result="OK"
+        else
+            result="ERROR"
+            diff ${statdump_filepath} ${demos_folder}/${demo}.txt >> toGround/doom.log 2>&1
+        fi
+    fi
+done
